6364155b141afd3847f283807809fb67a970ea11
ecz <ericocruz.cruz@proton.me>
2026-04-13 22:13:59 -0300

    Adicao do estilo para tabela

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -110,3 +110,57 @@ export const TabButton = styled.button`
 `
 
 
+export const Form = styled.form`
+  width: 100%;
+  max-width: 500px;
+  margin: 30px auto;
+
+ 
+  border-radius: 6px;
+  box-shadow: 0 0 10px rgba(67, 63, 63, 0.1);
+
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  gap:10px;
+  font-family: system-ui;
+
+  input{
+    border: 1px solid #e2dada;
+    border-radius: 4px;
+    height: 40px;
+    padding: 0 15px;
+    font-size: 16px;
+    
+  }
+  
+  &:focus{
+    border-color: #333;
+    outline: none;
+  }
+
+  label{
+    display: flex;
+    flex-direction: column;
+    font-family: system-ui;
+    font-weight: 600;
+    gap: 5px;
+  }
+
+  button {
+    margin-top: 10px;
+    height: 40px;
+    cursor: pointer;
+    background: ${primaryColor};
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    font-weight: bold;
+    transition: all 0.3s;
+  }
+
+  button:hover {
+    filter: brightness(1.2);
+  }
+  
+`
