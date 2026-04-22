7e94153e67503f7dd4cfaf1bf812930fef85b7b5
ecz <ericocruz.cruz@proton.me>
2026-04-22 08:01:36 -0300

    Feat:submenu style

--- src/pages/Stock/styled.js
+++ src/pages/Stock/styled.js
@@ -27,3 +27,49 @@ export const ProfilePicture = styled.div`
   }
 
 `
+
+export const MenuContainer = styled.div`
+  position: relative;
+  display: inline-block;
+ 
+`
+
+export const  MenuList = styled.ul`
+position:absolute;
+top: 100%;
+right: 0;
+background-color: #fffafa;
+min-width: 140px;
+box-shadow: 0px 8px 16px rgba(0,0,0, 0.1);
+border: 1px solid #ddd;
+border-radius: 4px;
+z-index: 1000;
+list-style: none;
+padding: 5px 0;
+margin 5px 0 0 0;
+
+`
+
+export const MenuItem = styled.li`
+padding: 8px 16px;
+cursor: pointer;
+font-size: 14px;
+transition: background 0.2s;
+color:#333;
+
+ &:hover{
+ background-color: ${primaryColor};
+ color: #fff;
+}
+`
+
+export const Divider = styled.li`
+height: 1px;
+background-color: #eee;
+margin: 4px 0;
+cursor: default;
+
+&: hover{
+background-color: #eee;
+}
+`
