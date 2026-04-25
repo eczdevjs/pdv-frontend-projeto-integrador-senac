188c1819b1742ec6a5078db6791733b681170405
ecz <ericocruz.cruz@proton.me>
2026-04-25 19:43:13 -0300

    Fix: Forcar o saga deletar dados de login localStorage

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export default axios.create({
+const api =  axios.create({
   baseURL: "http://192.168.0.37:3001",
 });
+
+api.interceptors.request.use((config)=>{
+  const token = localStorage.getItem('token');
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config;
+});
+
+export default api;
