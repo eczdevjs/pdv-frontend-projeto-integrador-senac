6b10acf6d66d5ab884f6ef6c5e8045fccc4b191e
ecz <ericocruz.cruz@proton.me>
2026-04-28 10:20:35 -0300

    FilterDate function correspondency

--- src/components/Layout/CashierModals/index.js
+++ src/components/Layout/CashierModals/index.js
@@ -2,14 +2,15 @@ import {OpenCashier} from './OpenCashier';
 import {CloseCashier} from './CloseCashier';
 import {Deposit} from './Deposit'
 import { Withdraw} from './Withdraw';
-
+import { FilterDate } from './FilterDate';
 
 
 const MODAL_COMPONENTS = {
     OPEN_CASHIER: OpenCashier,
     CLOSE_CASHIER: CloseCashier,
     DEPOSIT: Deposit,
-    WITHDRAW: Withdraw
+    WITHDRAW: Withdraw,
+    FILTER_DATE: FilterDate
 };
 
 export function CashierModalManager ({modalType, ...props}){
