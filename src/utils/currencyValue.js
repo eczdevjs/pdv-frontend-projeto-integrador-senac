d8ed2ad42d45149083a56015a8e5ce5d9a2ade71
ecz <ericocruz.cruz@proton.me>
2026-05-15 18:09:10 -0300

    currency switched to brazilian

--- src/utils/currencyValue.js
+++ src/utils/currencyValue.js
@@ -1,6 +1,6 @@
 export const toCurrency = (amount) => {
     if (isNaN(Number(amount))) return;
-    return Intl.NumberFormat('en-US', { style: "currency", currency: 'BRL' }).format(amount);
+    return Intl.NumberFormat('pt-BR', { style: "currency", currency: 'BRL' }).format(amount);
 }
 
 
