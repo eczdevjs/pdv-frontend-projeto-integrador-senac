a869b628728a658f152dcda80ab7dc89a76c90e1
ecz <ericocruz.cruz@proton.me>
2026-05-15 18:10:20 -0300

    Feat: implementarion brazilian date format

--- src/utils/toBrazilianDate.js
+++ src/utils/toBrazilianDate.js
@@ -1,3 +1,15 @@
-export  const toBrazilianDate = (isDate)=>{
+export const toBrazilianDate = (date) => {
     
+    if(!validate(date)) return;
+    
+    const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
+        dateStyle: 'short',
+        timeStyle: 'short'
+    });
+
+    return dateFormatter.format(date);
+}
+
+const validate = (date) => {
+    return date instanceof Date && !isNaN(date);
 }
