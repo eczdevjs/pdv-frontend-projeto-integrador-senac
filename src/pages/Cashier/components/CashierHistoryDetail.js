1e07300558c2c718739f20037cd006afa67c3b23
ecz <ericocruz.cruz@proton.me>
2026-05-15 18:11:52 -0300

    CashierHistoryDetail component

---
+++ src/pages/Cashier/components/CashierHistoryDetail.js
@@ -0,0 +1,112 @@
+import React, { use, useState } from "react";
+import axios from '../../../services/axios';
+import { useDispatch } from "react-redux";
+import { Container, Table, OrderInfo } from "../../../styles/GlobalStyle";
+import Loading from '../../../components/Loading';
+import { toast } from "react-toastify";
+import PropTypes from "prop-types";
+import * as actions from '../../../store/modules/auth/actions'
+import { toCurrency } from "../../../utils/currencyValue";
+import { toBrazilianDate } from "../../../utils/toBrazilianDate";
+import { useCashier } from "../../../Context/CashierContext";
+import { Link } from "react-router-dom";
+
+
+
+
+export default function CashierHistoryDetail({ match }) {
+    const [isLoading, setIsLoading] = useState(true);
+    const shiftId = match.params.shiftId;
+    console.log({ match })
+    console.log("shiftId : ", shiftId);
+    const [transactions, setTransactions] = useState([]);
+    const { handleGetTransactions, handleGetShift } = useCashier();
+    const [shift, setShift] = useState(null);
+
+    const dateFormatter = new Intl.DateTimeFormat('en-US', {
+        dateStyle: 'short',
+        timeStyle: 'short'
+    });
+
+
+    React.useEffect(() => {
+        if (!shiftId) return;
+        console.log('useEffect cashier detail called')
+        async function getTransactions() {
+            try {
+                const response = await handleGetTransactions(shiftId);
+                console.log("Response cashier detail transactions: ", response);
+                setTransactions(response);
+                const shift = await (handleGetShift(shiftId));
+                console.log("shiftRequest: ", shift)
+                setShift(shift);
+
+            } catch (error) {
+                toast.error('Error fetching cashier detail information');
+                console.log(error);
+            }
+        }
+        getTransactions();
+    }, [shiftId]);
+
+
+
+    return (
+        <div style={{ display: "flex", flexDirection: 'column' }}>
+
+            <h1>Detalhes Seção Caixa</h1>
+            <Container>
+
+                {shift && (<Container >
+                    <OrderInfo>
+                        <dt>Abertura</dt>
+                        <dd>{toBrazilianDate(new Date(shift.startTime))}</dd>
+
+                        <dt>Valor</dt>
+                        <dd>{toCurrency(shift.openingBalance)}</dd>
+
+                        <dt>Fechamento</dt>
+                        <dd>{dateFormatter.format(new Date(shift.endTime))}</dd>
+
+                        <dt>Valor</dt>
+                        <dd>{toCurrency(shift.closingBalance)}</dd>
+
+                        <dt>Diferença</dt>
+                        <dd>{toCurrency(shift.difference)}</dd>
+                    </OrderInfo>
+
+                    <div>
+                        <h2>Historico de transações</h2>
+                    </div>
+                    <Table>
+                        <thead>
+                            <tr key="headerTable">
+                                <th>Data / Hora</th>
+                                <th>Usuário</th>
+                                <th>Transação</th>
+                                <th>Valor</th>
+                                <th>Método</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {transactions && transactions.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{toBrazilianDate(new Date(item.createdAt))}</td>
+                                    <td>{item.user.name}</td>
+                                    <td>{item.type.name}</td>
+                                    <td>{toCurrency(item.amount)}</td>
+                                    <td>{item.payment.name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+
+                </Container>)}
+            </Container>
+        </div>
+    );
+}
+
+CashierHistoryDetail.prototype = {
+    match: PropTypes.shape({}).isRequired
+}
